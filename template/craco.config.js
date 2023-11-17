2e1b2e878a47fa51f4737c29433821f2d94f50a3
JSmith AR <91075107+smithb2chat@users.noreply.github.com>
2023-11-17 14:32:25 -0500

    enable HTTPS, custom build:success output

--- template/craco.config.js
+++ template/craco.config.js
@@ -1,8 +1,35 @@
+const chalk = require("chalk");
+const project = require("./package.json");
+
 module.exports = {
   devServer: (devServerConfig) => {
+    const widgetURL = "https://app.b2chat.io/agent/chat/?widgetdev=1";
     devServerConfig.open = {
       ...devServerConfig.open,
-      target: ["http://app.b2chat.io/agent/chat/?widgetdev=1"],
+      target: [widgetURL],
+    };
+    devServerConfig.onListening = (server) => {
+      server.compiler.hooks.done.tap("done", (stats) => {
+        const statsData = stats.toJson({
+          all: false,
+          warnings: true,
+          errors: true,
+        });
+        const isSuccessful =
+          !statsData.errors.length && !statsData.warnings.length;
+
+        if (isSuccessful) {
+          console.clear();
+          console.log(chalk.green("Compiled successfully!"));
+          console.log();
+          console.log(
+            `You can now view ${chalk.bold(project.name)} in the browser.`
+          );
+          console.log();
+          console.log(`  ${chalk.bold("On B2Chat console:")}  ${widgetURL}`);
+          console.log();
+        }
+      });
     };
     return devServerConfig;
   },
